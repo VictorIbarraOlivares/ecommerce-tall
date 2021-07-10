031204e11a3a9ec8ffe09376935093270d0170c7
unknown <victor.ibarra.olivares@gmail.com>
2021-07-10 09:45:40 -0400

    listas las seeds de category and subcategory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
