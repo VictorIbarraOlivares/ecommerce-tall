5326a42fd49727cd748a70c5c05505bda43b9fb2
unknown <victor.ibarra.olivares@gmail.com>
2021-07-10 08:49:26 -0400

    se crean las relaciones y una nueva tabla image

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,22 @@ class Category extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'slug', 'image', 'icon'];
+
+    // Relacion uno a muchos
+    public function subcategories()
+    {
+        return $this->hasMany(Subcategory::class);
+    }
+
+    // Relacion muchos a muchos
+    public  function brands()
+    {
+        return $this->belongsToMany(Brand::class);
+    }
+
+    // Relacion a traves de otra tabla intermedia
+    public function products()
+    {
+        return $this->hasManyThrough(Product::class, Subcategory::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function sizes()
+    {
+        return $this->hasMany(Size::class);
+    }
+
+    // Relacion uno a muchos inversa
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    // Relacion muchos a muchos
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class);
+    }
+
+    // relacion uno a muchos polimorfica
+    public function images()
+    {
+        return $this->morphToMany(Image::class, 'imageable');
+    }
 }
