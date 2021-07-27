5bdc2b9a6c675db93b05c12dfad3c4f66792d50a
unknown <victor.ibarra.olivares@gmail.com>
2021-07-27 17:18:36 -0400

    se incluye icono de lupa

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,4 +1,8 @@
-<div class="flex-1">
+<div class="flex-1 relative">
     {{-- The Master doesn't talk, he acts. --}}
     <x-jet-input type="text" class="w-full" placeholder="¿Estás buscando algún producto?" />
+
+    <button class="absolute top-0 right-0 w-12 h-full bg-orange-500 flex items-center justify-center rounded-r-md">
+        <x-search size="35" color="white" />
+    </button>
 </div>
