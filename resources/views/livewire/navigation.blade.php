54f088a23563a5554380f9ee134bc437b778466c
unknown <victor.ibarra.olivares@gmail.com>
2021-07-27 16:28:05 -0400

    arreglos

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,6 +1,6 @@
 <header class="bg-trueGray-700" >
     {{-- The best athlete wants his opponent at his best. --}}
-    <div class="container flex">
+    <div class="container flex ">
         <h1>Holaaaaa</h1>
     </div>
 </header>
