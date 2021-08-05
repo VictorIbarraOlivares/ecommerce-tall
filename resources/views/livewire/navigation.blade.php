92166c89c6bb6f62c6b824d5ca95fe91784a14d9
unknown <victor.ibarra.olivares@gmail.com>
2021-08-05 17:15:36 -0400

    se termina diseno del submenu y se agrega funcionalidad al boton de categorias

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,14 +1,8 @@
-<style>
-    #navigation-mmenu {
-        height: calc(100vh - 4rem);
-    }
-
-</style>
-
-<header class="bg-trueGray-700 sticky top-0">
+<header x-data="dropdown()" class="bg-trueGray-700 sticky top-0">
     {{-- The best athlete wants his opponent at his best. --}}
     <div class="container flex items-center h-16">
-        <a
+        <a x-on:click="show"
+            :class="{'bg-opacity-25' : !open, 'text-orange-500 bg-opacity-100' : open}"
             class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -79,25 +73,26 @@
         @livewire('dropdown-cart')
     </div>
 
-    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+    <nav id="navigation-menu" x-show="open" x-cloack class="bg-trueGray-700 bg-opacity-25 w-full absolute">
         <div class="container h-full">
-            <div class="grid grid-cols-4 h-full relative">
+            <div x-on:click.away="close" class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
                     @foreach ($categories as $category)
-                        <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                        <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
                             <a href="" class="py-2 px-4 text-sm flex items-center">
                                 <span class="flex justify-center w-9">{!! $category->icon !!}</span>
                                 {{ $category->name }}
                             </a>
 
-                            <div class="bg-red-500 absolute w-3/4 h-full top-0 right-0">
-
+                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                                <x-navigation-subcategories :category="$category" />
                             </div>
                         </li>
                     @endforeach
                 </ul>
 
                 <div class="col-span-3 bg-gray-100">
+                    <x-navigation-subcategories :category="$categories->first()" />
                 </div>
             </div>
         </div>
