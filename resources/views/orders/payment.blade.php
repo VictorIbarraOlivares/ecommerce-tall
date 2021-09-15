de64a9fe84f69fe6fe59f4242e302c0503b7ed7c
unknown <victor.ibarra.olivares@gmail.com>
2021-09-15 09:05:16 -0300

    se termina con las ordenes de pago

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -1,3 +1,91 @@
 <x-app-layout>
-    <h1>Paga lo que debes !</h1>
+    <div class="container py-8">
+        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
+            <p class="text-gray-700 uppercase">
+                <span class="font-bold">Número de orden:</span> Orden-{{ $order->id }}
+            </p>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
+            <div class="grid grid-cols-2 gap-6 text-gray-700">
+                <div class="">
+                    <p class="text-lg font-semibold uppercase">Envío</p>
+                    @if ($order->envio_type == 1)
+                        <p class="text-sm">Los productos deben ser recogidos en la tienda</p>
+                        <p class="text-sm">Calle falsa 1111</p>
+                    @else
+                        <p class="text-sm">Los productos serán enviados a:</p>
+                        <p class="text-sm">{{ $order->address }}</p>
+                        <p>{{ $order->department->name }} - {{ $order->city->name }} - {{ $order->district->name }}</p>
+                    @endif
+                </div>
+                <div>
+                    <p class="text-lg font-semibold uppercase">Datos de contacto</p>
+                    <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
+                    <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 mb-6 text-gray-700">
+            <p class="text-xl font-semibold mb-4">Resumen</p>
+
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Precio</th>
+                        <th>Cant</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody class="dibide-y divide-gray-300">
+                    @foreach (json_decode($order->content) as $item)
+                        <tr>
+                            <td>
+                                <div class="flex">
+                                    <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+                                    <article>
+                                        <h1 class="font-bold">{{ $item->name }}</h1>
+                                        <div class="flex text-xs">
+                                            @isset($item->options->color)
+                                                Color: {{ __($item->options->color) }}
+                                            @endisset 
+                                            @isset($item->options->size)
+                                                - {{ $item->options->size }}
+                                            @endisset
+                                        </div>
+                                    </article>
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                {{ $item->price }} USD
+                            </td>
+                            <td class="text-center">
+                                {{ $item->qty }}
+                            </td>
+                            <td class="text-center">
+                               {{ $item->price * $item->qty }} USD 
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 flex justify-between items-center">
+            <img class="h-14" src="{{ asset('img/metodo-pago.jpg') }}" alt="">
+            <div class="text-gray-700">
+                <p class="text-sm font-semibold ">
+                    Subtotal: {{ $order->total - $order->shipping_cost }} USD
+                </p>
+                <p class="text-sm font-semibold ">
+                    Envío: {{ $order->shipping_cost }} USD
+                </p>
+                <p class="text-lg font-bold uppercase">
+                    Total: {{ $order->total }} USD
+                </p>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
