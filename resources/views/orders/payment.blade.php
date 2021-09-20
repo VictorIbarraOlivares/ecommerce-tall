142e3eb65ad57208a6c880a906b882ebfb317e53
unknown <victor.ibarra.olivares@gmail.com>
2021-09-20 17:28:00 -0300

    se crear las variables con los datos de paypal en .env

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -33,94 +33,102 @@
         $preference->items = $products;
         $preference->save();
     @endphp
-    <div class="container py-8">
-        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
-            <p class="text-gray-700 uppercase">
-                <span class="font-bold">Número de orden:</span> Orden-{{ $order->id }}
-            </p>
-        </div>
 
-        <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
-            <div class="grid grid-cols-2 gap-6 text-gray-700">
-                <div class="">
-                    <p class="text-lg font-semibold uppercase">Envío</p>
-                    @if ($order->envio_type == 1)
-                        <p class="text-sm">Los productos deben ser recogidos en la tienda</p>
-                        <p class="text-sm">Calle falsa 1111</p>
-                    @else
-                        <p class="text-sm">Los productos serán enviados a:</p>
-                        <p class="text-sm">{{ $order->address }}</p>
-                        <p>{{ $order->department->name }} - {{ $order->city->name }} - {{ $order->district->name }}</p>
-                    @endif
+    <div class="grid grid-cols-5 gap-6 container py-8">
+        <div class="col-span-3">
+            <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
+                <p class="text-gray-700 uppercase">
+                    <span class="font-bold">Número de orden:</span> Orden-{{ $order->id }}
+                </p>
+            </div>
+    
+            <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
+                <div class="grid grid-cols-2 gap-6 text-gray-700">
+                    <div class="">
+                        <p class="text-lg font-semibold uppercase">Envío</p>
+                        @if ($order->envio_type == 1)
+                            <p class="text-sm">Los productos deben ser recogidos en la tienda</p>
+                            <p class="text-sm">Calle falsa 1111</p>
+                        @else
+                            <p class="text-sm">Los productos serán enviados a:</p>
+                            <p class="text-sm">{{ $order->address }}</p>
+                            <p>{{ $order->department->name }} - {{ $order->city->name }} - {{ $order->district->name }}</p>
+                        @endif
+                    </div>
+                    <div>
+                        <p class="text-lg font-semibold uppercase">Datos de contacto</p>
+                        <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
+                        <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
+                    </div>
                 </div>
-                <div>
-                    <p class="text-lg font-semibold uppercase">Datos de contacto</p>
-                    <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
-                    <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
-                </div>
+            </div>
+    
+            <div class="bg-white rounded-lg shadow-lg p-6 mb-6 text-gray-700">
+                <p class="text-xl font-semibold mb-4">Resumen</p>
+    
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Precio</th>
+                            <th>Cant</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody class="dibide-y divide-gray-300">
+                        @foreach (json_decode($order->content) as $item)
+                            <tr>
+                                <td>
+                                    <div class="flex">
+                                        <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+                                        <article>
+                                            <h1 class="font-bold">{{ $item->name }}</h1>
+                                            <div class="flex text-xs">
+                                                @isset($item->options->color)
+                                                    Color: {{ __($item->options->color) }}
+                                                @endisset 
+                                                @isset($item->options->size)
+                                                    - {{ $item->options->size }}
+                                                @endisset
+                                            </div>
+                                        </article>
+                                    </div>
+                                </td>
+                                <td class="text-center">
+                                    {{ number_format($item->price, 0, '', '.') }} CLP
+                                </td>
+                                <td class="text-center">
+                                    {{ $item->qty }}
+                                </td>
+                                <td class="text-center">
+                                   {{ number_format($item->price * $item->qty, 0, '', '.') }} CLP 
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
 
-        <div class="bg-white rounded-lg shadow-lg p-6 mb-6 text-gray-700">
-            <p class="text-xl font-semibold mb-4">Resumen</p>
-
-            <table class="table-auto w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Precio</th>
-                        <th>Cant</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody class="dibide-y divide-gray-300">
-                    @foreach (json_decode($order->content) as $item)
-                        <tr>
-                            <td>
-                                <div class="flex">
-                                    <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
-                                    <article>
-                                        <h1 class="font-bold">{{ $item->name }}</h1>
-                                        <div class="flex text-xs">
-                                            @isset($item->options->color)
-                                                Color: {{ __($item->options->color) }}
-                                            @endisset 
-                                            @isset($item->options->size)
-                                                - {{ $item->options->size }}
-                                            @endisset
-                                        </div>
-                                    </article>
-                                </div>
-                            </td>
-                            <td class="text-center">
-                                {{ number_format($item->price, 0, '', '.') }} CLP
-                            </td>
-                            <td class="text-center">
-                                {{ $item->qty }}
-                            </td>
-                            <td class="text-center">
-                               {{ number_format($item->price * $item->qty, 0, '', '.') }} CLP 
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-        <div class="bg-white rounded-lg shadow-lg p-6 flex justify-between items-center">
-            <img class="h-14" src="{{ asset('img/metodo-pago.jpg') }}" alt="">
-            <div class="text-gray-700">
-                <p class="text-sm font-semibold ">
-                    Subtotal: {{ number_format($order->total - $order->shipping_cost, 0, '', '.') }} CLP
-                </p>
-                <p class="text-sm font-semibold ">
-                    Envío: {{ number_format($order->shipping_cost, 0, '', '.') }} CLP
-                </p>
-                <p class="text-lg font-bold uppercase">
-                    Total: {{ number_format($order->total, 0, '', '.') }} CLP
-                </p>
+        <div class="col-span-2">
+            <div class="bg-white rounded-lg shadow-lg p-6 ">
+                <div class="flex justify-between items-center">
+                    <img class="h-10" src="{{ asset('img/metodo-pago.jpg') }}" alt="">
+                    <div class="text-gray-700">
+                        <p class="text-sm font-semibold ">
+                            Subtotal: {{ number_format($order->total - $order->shipping_cost, 0, '', '.') }} CLP
+                        </p>
+                        <p class="text-sm font-semibold ">
+                            Envío: {{ number_format($order->shipping_cost, 0, '', '.') }} CLP
+                        </p>
+                        <p class="text-lg font-bold uppercase">
+                            Total: {{ number_format($order->total, 0, '', '.') }} CLP
+                        </p>
+                        
+                    </div>
+                </div>
                 <div class="cho-container">
-
+        
                 </div>
             </div>
         </div>
@@ -143,7 +151,7 @@
             },
             render: {
                 container: '.cho-container', // Indica el nombre de la clase donde se mostrará el botón de pago
-                label: 'Pagar', // Cambia el texto del botón de pago (opcional)
+                label: 'Pagar (MercadoPago)', // Cambia el texto del botón de pago (opcional)
             }
         });
     </script>
