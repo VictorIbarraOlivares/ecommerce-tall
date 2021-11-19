5edd19c5926e87b0ab7cdf598e4690ae77469c4d
unknown <victor.ibarra.olivares@gmail.com>
2021-11-19 11:02:32 -0300

    se redirije a editar el producto despues de crearlo

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Livewire\Admin\CreateProduct;
+use App\Http\Livewire\Admin\EditProduct;
 use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('/products/create', CreateProduct::class)->name('admin.products.create');
-Route::get('/products/{product}/edit', function($id){
-    return $id;
-})
-->name('admin.products.edit');
+Route::get('/products/{product}/edit', EditProduct::class)->name('admin.products.edit');
