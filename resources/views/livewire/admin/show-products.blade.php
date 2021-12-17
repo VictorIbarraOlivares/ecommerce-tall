07928666b96245af9772655d83ed314d0ea15478
unknown <victor.ibarra.olivares@gmail.com>
2021-12-17 16:28:52 -0300

    cambiar estado del producto

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -79,7 +79,7 @@
                                 @switch($product->status)
                                     @case(App\Models\Product::BORRADOR)
                                         <span
-                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-red-800">
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                                             Borrador
                                         </span>
                                         @break
